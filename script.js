9b23f3c823d69731f05e24b7942c382b80ac24aa
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2023-10-04 15:03:00 +0100

    Created JS logic for contact form, host mail server and messages displayed upon invalid input

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // code for home page content container
 
 const observer = new IntersectionObserver((entries) => {
@@ -23,3 +24,33 @@ const progressBars = document.querySelectorAll('.graph-progress');
 progressBars.forEach((bar, index) => {
   bar.style.width = `${progressValues[index]}%`;
 });
+
+
+// code for contact form/ routes to personal email
+// ADD EMAIL DETAILS ONCE PROJECT COMPLETED
+
+const sendEmail = (name, company, email, message) => {
+    email.send({
+      Host: "smtp.gmail.com",
+      Username: "your-email-address", // Replace with your own email address
+      Password: "your-email-password", // Replace with your own email password
+      To: "your-email-address@example.com", // Replace with your own email address
+      From: "your-email-address@gmail.com", // Replace with your own email address
+      Subject: "New message from contact form",
+      Body: `Name: ${name}\nCompany: ${company}\nEmail: ${email}\nMessage: ${message}`,
+    }).then((message) => {
+      alert("Thank you for your message!");
+    });
+  };
+  
+  const contactForm = document.getElementById("contact-form");
+  contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const company = document.getElementById("company").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    sendEmail(name, company, email, message);
+    contactForm.reset();
+  });
+
