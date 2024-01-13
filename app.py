14ef9f7441919a694155fe7d417c6ebb60f7bde3
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2024-01-13 20:11:49 +0000

    Finalised app for routing messages from contact form to gmail client

--- app.py
+++ app.py
@@ -14,3 +14,26 @@ app.config['MAIL_USE_TLS']= False
 
 mail = Mail(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# This route sends a message to the email address provided in the contact form
+@app.route('/send-mail, methode=[POST]')
+def send_email():
+    name = request.form['name']
+    company = request.form['company']
+    email = request.form['email']
+    message = request.form['message']
+
+    msge = Message("New messsage from contact form at Portfolio",
+                    sender="email",
+                    recipients=['chefjoemiller@gmail.com'])
+    msge.body = f"Name: {name}\nCompany: {company}\nEmail: {email}\n\n{message}"
+
+    mail.send(msg)
+
+    return 'Message sent!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
