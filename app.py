3e2dc725644f0041ed8ac54ae1fe73d11a3ab296
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2024-01-15 19:12:43 +0000

    Created route structure for handling contact form data. Further data added securely

--- app.py
+++ app.py
@@ -8,5 +8,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    name = request.form['name']
+    company = request.form['company']
+    email = request.form['email']
+    message = request.form['message']
+
+    msg = Message("New Message from Portfolio site",
+                  sender="REPLACE_WITH_YOUR_EMAIL",
+                  recipients=["REPLACE_WITH_YOUR_EMAIL"])
+    msg.body = f"""
+    From: {name} <{email}>
+    Company: {company}
+
+    {message}
+    """
+
+    mail.send(msg)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
