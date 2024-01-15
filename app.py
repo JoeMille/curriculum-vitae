6309c4cc1e0035b4763993ec75fad4a1766788f6
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2024-01-15 19:23:34 +0000

    Added app configs for rerouting messages to external mail server

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+# Email configuration
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "REPLACE_WITH_YOUR_EMAIL"
+app.config['MAIL_PASSWORD'] = "REPLACE_WITH_YOUR_PASSWORD"
+
+mail = Mail(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Email Config
 @app.route('/send_email', methods=['POST'])
 def send_email():
     name = request.form['name']
