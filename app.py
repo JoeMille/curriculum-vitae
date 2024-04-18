ba8c24c142d473c4360d53076db96bfb0066cc90
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2024-04-18 09:22:09 +0100

    Push to heroku

--- app.py
+++ app.py
@@ -16,29 +16,6 @@ def index():
 def projects():
     return render_template('projects.html')
 
-@app.route('/contact', methods=['POST'])
-def contact():
-    name = request.form.get('name')
-    company = request.form.get('company')
-    email = request.form.get('email')
-    message_body = request.form.get('message')
-
-    print(f'Form data: {name}, {company}, {email}, {message_body}')
-
-    message = Mail(
-        from_email='chefjoemiller1992@gmail.com',
-        to_emails='chefjoemiller1992@gmail.com',
-        subject='New contact form submission',
-        html_content=f'From: {name}<br>Company: {company}<br>Email: {email}<br><br>{message_body}'
-    )
-    try:
-        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
-        response = sg.send(message)
-        print(f'SendGrid response: {response.status_code}, {response.body}')
-        return 'Form submitted'
-    except Exception as e:
-        print(f'Error: {e}')
-        return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
